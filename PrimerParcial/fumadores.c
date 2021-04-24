81bbea6780de0a8e145534c1e433253533258b72
ItzelCabrera <icabrerav1600@alumno.ipn.mx>
2021-04-23 21:50:25 -0500

    Add program of smokers

--- PrimerParcial/fumadores.c
+++ PrimerParcial/fumadores.c
@@ -9,9 +9,6 @@
 #include<sys/types.h>
 #include<sys/wait.h>
 #include<pthread.h>
-#define I1 'Cerillo'
-#define I2 'Papel'
-#define I3 'Tabaco'
 
 sem_t fC;
 sem_t fP;
@@ -30,7 +27,7 @@ int main(){
     sem_init(&fC,0,0);
     sem_init(&fP,0,0);
     sem_init(&fT,0,0);
-    sem_init(&A,0,1);
+    sem_init(&a,0,1);;
 
     pthread_create(&FC,0,&Fumadores,(void*)(intptr_t)1);
     pthread_create(&FP,0,&Fumadores,(void*)(intptr_t)2);
@@ -49,76 +46,67 @@ int main(){
 
 }
 
-/*void *FumadorCerillo(void *arg){
-    pthread_exit(NULL);
-}
-
-void *FumadorPapel(void *arg){
-    pthread_exit(NULL);
-}
-
-void *FumadorTabaco(void *arg){
-    pthread_exit(NULL);
-}*/
-
 void *Fumadores(void *arg){
     int f = (intptr_t)arg;
-    printf("Entra fumador%d\tPID = %ld\n",f,pthread_self());
-
+    
     while(1){
         switch(f){
             case 1:
-                printf("\tFC a mimir\n");
-                sem_wait(&fC);   
-                printf("...FUMADOR CERILLO FUMA...\n");
+                printf("\tFumador con cerillo espera a que le asignen su turno\n");
+                sem_wait(&fC); //espera a que el agente le de permiso
+                printf("...FUMADOR CERILLO FUMA...\tPID = %ld\n",pthread_self());
                 break;
             case 2:
-                printf("\tFP a mimir\n");
-                sem_wait(&fP);
-                printf("...FUMADOR PAPEL FUMA...\n");
+                printf("\tFumador con papel espera a que le asignen su turno\n");
+                sem_wait(&fP);//espera a que el agente le de permiso
+                printf("...FUMADOR PAPEL FUMA...\tPID = %ld\n",pthread_self());
                 break;
             case 3:
-                printf("\tFT a mimir\n");
-                sem_wait(&fT);
-                printf("...FUMADOR TABACO FUMA...\n");
+                printf("\tFumador con tabaco espera a que le asignen su turno\n");
+                sem_wait(&fT);//espera a que el agente le de permiso
+                printf("...FUMADOR TABACO FUMA...\tPID = %ld\n",pthread_self());
                 break;
             default:
                 printf("Error con el fumador\n");
         }
-        sem_post(&a);
+        sem_post(&a); //permite el paso al agente para que vuelva a generar dos ingredientes
     }
     pthread_exit(NULL);
 }
 
 void *Agente(void *arg){  
     int aleat = 0;  
-    printf("Entra agente\tPID = %ld\n",pthread_self());
+    int c = 0; //validacion para cuando entra por primera vez al agente
+    srand(time(0));
+
     while(1){
-        sem_wait(&a);
-        //genera un numero aleatorio entre 1 y 3
-        aleat = rand()%3;        
+        sleep(4);
+        if(c==0)sem_wait(&a);//cuando el agente entra por primera vez, se actualiza su valor a cero, para poder pausar y renaudar al agente
+        aleat = rand()%3; 
+        //0 --> paso al fumador con cerillo 
+        //1 --> paso al fumador con papel 
+        //2 --> paso al fumador con tabaco 
+    
         switch(aleat){
+            case 0:
+                printf("¡Ingredientes generados [Papel] y [Tabaco]!\n");
+                sem_post(&fC); //permite que fume el fumador con cerillo
+                break;
             case 1:
-                printf("¡Ingredientes generados [%s] y [%s]!\n",I2,I3);
-                sem_wait(&a);
-                printf("\tA a mimir\n");
-                sem_post(&fC);
+                printf("¡Ingredientes generados [Cerillo] y [Tabaco]!\n");
+                sem_post(&fP);//permite que fume el fumador con papel
                 break;
             case 2:
-                printf("¡Ingredientes generados [%s] y [%s]!\n",I1,I3);
-                sem_wait(&a);
-                printf("\tA a mimir\n");
-                sem_post(&fP);
-                break;
-            case 3:
-                printf("¡Ingredientes generados [%s] y [%s]!\n",I1,I2);
-                sem_wait(&a);
-                printf("\tA a mimir\n");
-                sem_post(&fT);
+                printf("¡Ingredientes generados [Cerillo] y [Papel]!\n");
+                sem_post(&fT);//permite que fume el fumador con tabaco
                 break;
             default:
                 printf("Error al generar ingredientes\n");
+                break;
         }
+        printf("\tAgente detiene la produccion de recursos\n");
+        sem_wait(&a);
+        c=1; //el agente ya entró por primera vez
     }
     pthread_exit(NULL);
 }
